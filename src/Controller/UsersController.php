82bcfacdba1225766f55acfd3c8c0babf54315f2
BigBen83 <benjamin.toubhans@orange.fr>
2022-10-26 20:30:31 +0200

    form and fails 1

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Form\UsersType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/users')]
+class UsersController extends AbstractController
+{
+    #[Route('/index', name: 'users.index')]
+    public function index(): Response
+    {
+        return $this->render('users/index.html.twig', [
+            'controller_name' => 'UsersController',
+        ]);
+    }
+
+    #[Route('/login', name: 'users.login')]
+    public function login(): Response
+    {
+        return $this->render('users/login.html.twig', [
+            'controller_name' => 'UsersController',
+        ]);
+    }
+
+    #[Route('/register', name: 'users.register')]
+    public function register(
+        Users $user = null,
+        ManagerRegistry $doctrine,
+        Request $request
+        ): Response
+    {
+        $user = new Users();
+        $form = $this->createForm(UsersType::class, $user);
+        $form->remove('created');
+        $form->remove('role');
+        $form->remove('lastlogin');
+        $form->handleRequest($request);
+
+        
+        if($request->getMethod()=='POST') //penser à vérifier que les données sont valides
+        {
+            $user=$form->getData();
+            return $this->render('users/login.html.twig', [
+                
+            ]);   
+        }else{
+            return $this->render('users/register.html.twig', [
+                'form' => $form->createView()
+            ]);    
+        }
+    }
+}
---
+++ src/Form/UsersType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email')
+            ->add('address')
+            ->add('password')
+            ->add('created')
+            ->add('role')
+            ->add('lastlogin')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
