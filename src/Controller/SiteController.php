64da48ad334ce6ae8e3c014f002c0969253b7bfa
yves40 <yves.toubhans@free.fr>
2022-11-14 19:32:31 +0100

    Test some twig translations

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\LocaleSwitcher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SiteController extends AbstractController
 {
+    public function __construct(private LocaleSwitcher $localeSwitcher) { }
+
+
     #[Route('/', name: 'home')]
     public function home(): Response
     {
+        // $this->localeSwitcher->setlocale('en');
         return $this->render('home.html.twig', []);
     }
 
