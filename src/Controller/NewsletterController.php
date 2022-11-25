393428298d6f9e2b70ab6a047b02d5fef55c4397
BigBen83 <benjamin.toubhans@orange.fr>
2022-11-25 10:53:21 +0100

    controller

---
+++ src/Controller/NewsletterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewsletterController extends AbstractController
+{
+    #[Route('/newsletter', name: 'newsletter.send')]
+    public function index(): Response
+    {
+        return $this->render('newsletter/index.html.twig', [
+            'controller_name' => 'NewsletterController',
+        ]);
+    }
+}
