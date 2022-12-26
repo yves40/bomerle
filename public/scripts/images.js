cca5b59f55eb990861030081afb0109093a0e60d
yves40 <yves.toubhans@free.fr>
2022-12-26 18:40:00 +0100

    WIP on properties and image load process

--- public/scripts/images.js
+++ public/scripts/images.js
@@ -1,51 +1,48 @@
 // ------------------ Init loop to trap all mouse clicks -------------------------
 $(document).ready(function () {
-    console.log(getStaticProperty('version'));
-    console.log('Images loader');
-    let sessiondelay  = sessionStorage.getItem("sessiondelay");
-        if(sessiondelay === null) { 
-            sessiondelay = get('imageloadingdelay');      // Default delay to be used between images load
-            sessionStorage.setItem('sessiondelay', sessiondelay);
-            sessionStorage.setItem('checkdelay', true);
-            console.log('Session delay has been set to ' + sessiondelay + ' msec');
-        }
-        else {
-            console.log('Session previously set to ' + sessiondelay + ' msec');
-        }
-        // -------------------------
-        // Load document images
-        // -------------------------
-        let nbimages = 0;
-        $("img").each(function (indexInArray, element) { 
-            ++ nbimages;
-            let url = element.src;
-            let filename = url.replace(/^.*[\\\/]/, '')
-            let elemwidth = element.width;
-            let elemheight = element.height;
-            element.src = "/images/gif/loading.gif";
-            $(element).height("20px").width("20px").attr( { 'object-fit': 'cover', 'object-position': 'center'} );
-            $(element).on("load", () => {     // Here we track the end of the wait gif load
-                // Will load images with a delay
-                timeoutid = setTimeout( () => {
-                    $(element).off("load");     // Removes event handlers that were attached with .on()
-                    getImage(element, url)      // Load the real image
-                        .then( (timer) => {
-                            $(element).height(elemheight).width(elemwidth);
-                            let elapsed = timer.getElapsedString();
-                            let timestamp = timer.getTime();
-                            console.log(`${timestamp} Image Index : ${indexInArray} loaded ${filename} in ${elapsed}`)
-                        })
-                        .catch( (message) => {
-                            console.log(`Error : ${message}`)
-                        })
+    $props.load();
+    console.log('Application version : ' + $props.version());
+    const handlerversion = $props.imagehandler();
+    let imagedelay  = $props.imageloadingdelay();
+    console.log(`${handlerversion} : Delay between images currently set to ${imagedelay} msec`);
+    console.log(`You loaded ${$props.imageloadcount()} images in this session`);
+    // -------------------------
+    // Load document images
+    // -------------------------
+    let nbimages = 0;
+    $("img").each(function (indexInArray, element) { 
+        ++nbimages;
+        let url = element.src;
+        let filename = url.replace(/^.*[\\\/]/, '')
+        let elemwidth = element.width;
+        let elemheight = element.height;
+        element.src = "/images/gif/loading.gif";
+        $(element).height("20px").width("20px").attr( { 'object-fit': 'cover', 'object-position': 'center'} );
+        $(element).on("load", () => {     // Here we track the end of the wait gif load
+            // Will load images with a delay
+            timeoutid = setTimeout( () => {
+                $(element).off("load");     // Removes event handlers that were attached with .on()
+                getImage(element, url)      // Load the real image
+                    .then( (timer) => {
+                        $(element).height(elemheight).width(elemwidth);
+                        let elapsed = timer.getElapsedString();
+                        // Ajust timeout delay for the next image loop
+                        $props.set('imageloadingdelay', ($props.imageloadingdelay() + timer.getElapsed()) / 2);
+                        $props.set('imageloadcount', $props.imageloadcount() + 1);
+                        $props.save();
+                        let timestamp = timer.getTime();
+                        console.log(`${timestamp} Image Index : ${indexInArray} loaded ${filename} in ${elapsed}`)
+                    })
+                    .catch( (message) => {
+                        console.log(`Error : ${message}`)
+                    })
 
-                }, 
-                sessiondelay * indexInArray, url, filename, indexInArray);
-            })
-        });
-        console.log(`Loading ${nbimages} images`);
-    }
-)
+            }, 
+            imagedelay * indexInArray, url, filename, indexInArray);
+        })
+    });
+    console.log(`Loading ${nbimages} images`);
+})
 
 function getImage(element, url) {
     return new Promise((resolve, reject) => {
@@ -64,8 +61,3 @@ function getImage(element, url) {
             })
     });
 }
-
-// if(sessionStorage.getItem('checkdelay')) {
-//     sessionStorage.setItem('checkdelay', false);
-//     sessionStorage.setItem('sessiondelay', timer.getElapsed() * 1.1); // New timeout
-// }
--- public/scripts/properties.js
+++ public/scripts/properties.js
@@ -3,28 +3,29 @@
 //
 //    Dec 26 2022 Initial
 //----------------------------------------------------------------------------
-const allprops = {
-  "version": 'bomerle:1.51, Dec 26 2022 ',
-  "copyright": 'Ratoon software Inc, Chabreloche Corporation'
-}
-let dynamic = {
-  "imageloadingdelay" : 1000,
-  "imageloadcount" : 0,
-}
-// ------------------------------------------------------------------
-// Accessor for global properties
-// ------------------------------------------------------------------
-function getStaticProperty(propertyname) {
-  return allprops[propertyname] === undefined ? 
-      `${propertyname} : unknown property` : allprops[propertyname];
-}
-// ------------------------------------------------------------------
-// Accessors for dynamic properties
-// ------------------------------------------------------------------
-function get(propertyname) {
-  return dynamic[propertyname] === undefined ? 
-      `${propertyname} : unknown property` : dynamic[propertyname];
-}
-function set(propertyname, value) {
-  
-}
+const $props = ( () => {
+  let allprops = {
+    version : 'bomerle:1.00, Dec 26 2022 ',
+    copyright:  'Ratoon software Corporation Inc, Chabreloche France ',
+    imagehandler: 'images.js Dec 26 2022, 1.21',
+    'imageloadingdelay' : 1000,
+    'imageloadcount' :0
+  }
+
+  return {
+    version: () => { return allprops.version; },
+    copyright: () => { return allprops.copyright; },
+    imageloadingdelay: () => { return allprops['imageloadingdelay']; },
+    imageloadcount: () => { return allprops['imageloadcount']; },
+    imagehandler: () => { return allprops.imagehandler; },
+    set: (propertyname, value) => { allprops[propertyname] = value; },
+    get: (propertyname) => { return allprops[propertyname]; },
+    save: () => { sessionStorage.setItem('allprops', JSON.stringify(allprops)); },
+    load: () => {
+      let savedprops = sessionStorage.getItem("allprops");
+      if( savedprops !== null) {
+        allprops = JSON.parse(savedprops);
+      }
+     }
+  }
+})();
