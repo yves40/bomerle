568c40df0c9c52e989617228b79f708665db666f
yves40 <yves.toubhans@free.fr>
2023-02-08 18:45:24 +0100

    Now must proceed to intensive tests

--- public/scripts/knife.js
+++ public/scripts/knife.js
@@ -3,13 +3,6 @@ $(document).ready(function () {
     $props.load();
     console.log(`[${$props.version()} ]` );
     // -------------------------
-    // Check knife images
-    // -------------------------
-    let nbimages = 0;
-    $("img").each(function (indexInArray, element) {
-        ++nbimages;
-    });
-    // -------------------------
     // Arm click handlers
     // -------------------------
     armIcons();
@@ -67,12 +60,9 @@ function moveRight(element) {
     let url = $(element).attr('href');
     // Get the selected action element
     let selectedimageid = $(element).attr('id').split('-')[2];
-    // Build the current image list
-    let imagespayload = [];
-    $(".container img").each(function (indexInArray, theimage) {
-        imagespayload.push(getImageAtributes(theimage, selectedimageid, RIGHTACTION));
-    });
-    moveImage(imagespayload, url);
+    // Build the current image list and move the image
+    moveImage(buildImagesList(selectedimageid, RIGHTACTION), url);
+    // buildImagesList(selectedimageid, RIGHTACTION);
 }
 // ------------------------------------------------------------- Right handler 
 function moveLeft(element) {
@@ -80,11 +70,18 @@ function moveLeft(element) {
     // Get the selected action element
     let selectedimageid = $(element).attr('id').split('-')[2];
     // Build the current image list
+    moveImage(buildImagesList(selectedimageid, LEFTACTION), url);
+    // buildImagesList(selectedimageid, LEFTACTION);
+}
+// -------------------------------------------------------------
+// Build the images refresh list from the DOM
+// -------------------------------------------------------------
+function buildImagesList(selectedimageid, action) {
     let imagespayload = [];
-    $(".container img").each(function (indexInArray, theimage) {
-        imagespayload.push(getImageAtributes(theimage, selectedimageid, LEFTACTION));
+    $(".container img").each(function (idx, theimage) {
+        imagespayload.push(getImageAtributes(theimage, selectedimageid, action));
     });
-    moveImage(imagespayload, url);
+    return imagespayload;
 }
 // -------------------------------------------------------------
 // Helpers section
@@ -108,7 +105,11 @@ function getImageAtributes(element, selectedimageid, requestedaction) {
 }
 // ------------------------------------------------------------- Move the selected image
 function moveImage(imageslist, url) {
+    // console.log("Enter move Image");
+    // console.log(imageslist);
     let feedbackmessage = $('#feedback');
+    let movingimageid = 0;
+    let relatedimageid = 0;
     for(index = 0; index < imageslist.length; ++index) {
         let element = imageslist[index];
         if(element.action !== NOACTION) {   
@@ -128,11 +129,14 @@ function moveImage(imageslist, url) {
                 imageslist[index] = imagetarget;
                 imageslist[index-1] = imagemoved;
             }
-            break;
+            relatedimageid = imagetarget.imageid;
+            movingimageid = element.imageid;
+        break;
         }
     }
+    swapImages(movingimageid, relatedimageid);
+    // reloadImages(imageslist);
     // ----------------------------- Server DB update
-    reloadImages(imageslist);
     let payload = {
         "imagedata" :  imageslist
     }
@@ -154,7 +158,100 @@ function moveImage(imageslist, url) {
         }
     });
 }
-// ------------------------------------------------------------- Move the selected image
+// -------------------------------------------------------------
+// This method work only on images being moved
+// -------------------------------------------------------------
+function swapImages(movingimageid, relatedimageid) {
+    // Manage 2 cards, 2 images and 6 command icons
+    let card1 = null;
+    let card2 = null;
+    let img1 = img2 = null;
+    let left1, del1, right1, left2, del2, right2 = null;
+    $(`#imgcard-${movingimageid}`).each((idx, element) => {
+        card1 = element;
+    });
+    $(`#imgcard-${relatedimageid}`).each((idx, element) => {
+        card2 = element;
+    });
+    $(`#imgcard-${movingimageid} img`).each((idx, element) => {
+        img1 = element;
+    });
+    $(`#imgcard-${relatedimageid} img`).each((idx, element) => {
+        img2 = element;
+    });
+    $(`#imgcard-${movingimageid} a[id^=left]`).each((idx, element) => {
+        left1 = element;
+    });
+    $(`#imgcard-${movingimageid} a[id^=del]`).each((idx, element) => {
+        del1 = element;
+    });
+    $(`#imgcard-${movingimageid} a[id^=right]`).each((idx, element) => {
+        right1 = element;
+    });
+    $(`#imgcard-${relatedimageid} a[id^=left]`).each((idx, element) => {
+        left2 = element;
+    });
+    $(`#imgcard-${relatedimageid} a[id^=del]`).each((idx, element) => {
+        del2 = element;
+    });
+    $(`#imgcard-${relatedimageid} a[id^=right]`).each((idx, element) => {
+        right2 = element;
+    });
+
+    $(card1).hide();
+    $(card2).hide();
+
+    // Swap images and their data
+    let imgid1 = $(img1).attr('data-imageid');
+    let imgfile1 = $(img1).attr('data-imagefile');
+    let imgknifeid1 = $(img1).attr('data-imageknifeid');
+    let imgrank1 = $(img1).attr('data-imagerank');
+
+    let imgid2 = $(img2).attr('data-imageid');
+    let imgfile2 = $(img2).attr('data-imagefile');
+    let imgknifeid2 = $(img2).attr('data-imageknifeid');
+    let imgrank2 = $(img2).attr('data-imagerank');
+
+    $(img1).attr('src', '/images/knife/' + imgfile2);
+    $(img1).attr('data-imageid', imgid2);
+    $(img1).attr('data-imagefile', imgfile2);
+    $(img1).attr('data-imageknifeid', imgknifeid2);
+    $(img1).attr('data-imagerank', imgrank2);
+    
+    $(img2).attr('src', '/images/knife/' + imgfile1);
+    $(img2).attr('data-imageid', imgid1);
+    $(img2).attr('data-imagefile', imgfile1);
+    $(img2).attr('data-imageknifeid', imgknifeid1);
+    $(img2).attr('data-imagerank', imgrank1);
+    // Swap cards ID
+    $(card1).attr('id', `imgcard-${imgid2}`);
+    $(card2).attr('id', `imgcard-${imgid1}`);
+    // Swap command icons
+    let atr1 = $(left1).attr('id');  // LEFT (change ID)
+    let atr2 = $(left2).attr('id');
+    $(left1).attr('id', atr2);
+    $(left2).attr('id', atr1);
+    atr1 = $(del1).attr('href');     // DEL (change ID and URL)
+    atr2 = $(del2).attr('href');
+    $(del1).attr('href', atr2);
+    $(del2).attr('href', atr1);
+    atr1 = $(del1).attr('id');
+    atr2 = $(del2).attr('id');
+    $(del1).attr('id', atr2);
+    $(del2).attr('id', atr1);
+    atr1 = $(right1).attr('id');     // RIGHT (Change ID)
+    atr2 = $(right2).attr('id');
+    $(right1).attr('id', atr2);
+    $(right2).attr('id', atr1);
+
+    // Redisplay cards
+    $(`#imgcard-${movingimageid}`).slideDown(1000);
+    $(`#imgcard-${relatedimageid}`).slideDown(1000);
+    return;
+}
+// ------------------------------------------------------------- 
+// Refresh all the images
+// ------------------------------------------------------------- 
 function reloadImages(imageslist) {
     console.log(`********************* Reloading ${imageslist.length} image(s)`);
     $("#refreshzone").hide();
@@ -166,6 +263,21 @@ function reloadImages(imageslist) {
 
     // Images load
     imageslist.forEach((imgcard, index) => {
+        // Build the image card container
+        let newdiv = buildCard(imgcard, index, imageslist.length - 1);
+        // Insert the whole card
+        $("#refreshzone .row").append(newdiv);
+        console.log(`Added image card ${imgcard.imageid}`);
+    });
+    $("#refreshzone").slideDown(500);
+    armIcons();
+}
+// ------------------------------------------------------------- 
+// Build a card container for image and commands icons
+// In the initial version, this function is used to reload all
+// images after a move.
+// ------------------------------------------------------------- 
+function buildCard(imgcard, index, lastimageindex) {
 
         // Prepare command icons
         let lefticonbutton = document.createElement("a");
@@ -179,10 +291,9 @@ function reloadImages(imageslist) {
         let righticonbutton = document.createElement("a");
         let righticon = document.createElement("ion-icon");
         righticon.setAttribute('name', 'arrow-forward-circle-outline');
-        
-        // Build the card container
+
+        // The image card        
         let newdiv = document.createElement("div");
-        // Outer div
         newdiv.id = "imgcard-" + imgcard.imageid;
         newdiv.className = "col-sm";
         // The command icon zone
@@ -216,7 +327,7 @@ function reloadImages(imageslist) {
         deletebutton.appendChild(deleteicon);
         command.appendChild(deletebutton);
         // Right icon
-        if(index !== imageslist.length - 1) { // Right icon
+        if(index !== lastimageindex) { // Right icon
             righticonbutton.setAttribute('id', "right-" + imgcard.knifeid + "-"
                                                  + imgcard.imageid + "-"
                                                  + imgcard.rank);
@@ -226,12 +337,7 @@ function reloadImages(imageslist) {
         }
         // Insert the icons
         newdiv.appendChild(command);
-        // Insert the whole card
-        $("#refreshzone .row").append(newdiv);
-        console.log(`Added image card ${imgcard.imageid}`);
-    });
-    $("#refreshzone").slideDown(500);
-    armIcons();
+        return newdiv;
 }
 // ------------------------------------------------------------- Set up icons handlers
 function armIcons() {
