5ac11018297596207b3506024e48230574f8165a
yves40 <yves.toubhans@free.fr>
2023-02-16 12:57:16 +0100

    Version

--- public/scripts/properties.js
+++ public/scripts/properties.js
@@ -6,14 +6,15 @@
 //    Feb 04 2023 WIP on knife images administration
 //    Feb 13 2023 Image loading delay removed
 //    Feb 15 2023 WIP on date input in TWIG form
+//    Feb 16 2023 WIP on date input in TWIG form
 //----------------------------------------------------------------------------
 const $props = ( () => {
   const allprops = {
-    version : 'bomerle:1.11, Feb 15 2023 ',
+    version : 'bomerle:1.12, Feb 16 2023 ',
     copyright:  'Ratoon software Corporation Inc, Chabreloche France ',
     imagehandler: 'images.js Feb 13 2023, 1.25 ',
     knifehandler: 'knife.js Feb 08 2023, 1.05 ',
-    datehandler: 'twigdate.js Feb 15 2023, 1.01 ',
+    datehandler: 'twigdate.js Feb 16 2023, 1.02 ',
     knivesimageslocation: '%kernel.project_dir%/public/images/knife'
   }
   let dynprops = {
