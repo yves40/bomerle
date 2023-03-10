305395bf2fec3fab603fc45c677388a24eddb101
yves40 <yves.toubhans@free.fr>
2023-03-10 18:22:37 +0100

    Show Hiode details works

--- public/scripts/logs.js
+++ public/scripts/logs.js
@@ -3,13 +3,17 @@ $(document).ready(function () {
     $props.load();
     let zoomstate = false;
     const zoom = $(".msgzoom");
+    const showall = $('#showall');
+    const hideall = $('#hideall');
     const messagezone = $(".msgdetails");
 
     $(messagezone).hide();
     zoom.click(function (e) { 
         e.preventDefault();
         zoomMessage(this);
-    });;
+    });
+    showall.click( (e) => { e.preventDefault(); showAll()});
+    hideall.click( (e) => { e.preventDefault(); hideAll()});
 
     // ----------------------------------------------------------------------------
     // Zoom in, zoom out for log message details
@@ -26,6 +30,26 @@ $(document).ready(function () {
             $(msgzone).data('visible', true);
             $(msgzone).show();
         }     
+    }
+    // ----------------------------------------------------------------------------
+    // Show all messages details
+    // ----------------------------------------------------------------------------
+    function showAll() {
+        $('.msgdetails').each(function (index, element) {
+            // element == this
+            $(this).data('visible', true);
+            $(this).show();            
+        });
+    }
+    // ----------------------------------------------------------------------------
+    // Hide all messages details
+    // ----------------------------------------------------------------------------
+    function hideAll() {
+        $('.msgdetails').each(function (index, element) {
+            // element == this
+            $(this).data('visible', false);
+            $(this).hide();            
+        });
     }
 })
 
