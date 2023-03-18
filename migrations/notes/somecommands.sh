4772200a655e46c8fec1a076bd1f26c6075d1d65
yves40 <yves.toubhans@free.fr>
2023-03-18 11:06:36 +0100

    Some tests with doctrine command line

---
+++ migrations/notes/somecommands.sh
@@ -0,0 +1,12 @@
+# Migrate new entity attributes
+php bin/console make:migration
+php bin/console doctrine:migrations:migrate
+# Migrations status
+php bin/console doctrine:migrations:status
+# undo a migration
+php bin/console doctrine:migrations:execute DoctrineMigrations\Version20230318092754 --down
+# Delete a migration
+# The operation takes place in the version table of the supporting DB
+# Does not take care of the migration file. 
+# Delete it manually otherwise the doctrine status will be wrong
+php bin/console doctrine:migrations:version Version20230318092754 --delete
