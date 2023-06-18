1db320249717fa57ba2a38fc9b80e0e4e5c17a6c
yves40 <yves.toubhans@free.fr>
2023-06-18 19:51:22 +0200

    Gallery UI, Phase 2

--- public/scripts/classes/gallery.js
+++ public/scripts/classes/gallery.js
@@ -2,36 +2,41 @@
     Gallery
 
     jun 17 2023     Initial
+    jun 18 2023     Add div and text to the gallery template
 
 ----------------------------------------------------------------------------*/
 class Gallery {
 
     constructor(container) {
         // Init
-        this.version = 'Gallery:1.00, Jun 17 2023 ';
+        this.version = 'Gallery:1.01, Jun 18 2023 ';
         this.container = container;
         this.containername = $(container).attr('name');
-        this.galleryzone = `${this.containername}-zone`;
+        this.gallery = `${this.containername}-zone`;
         this.galleryarea = `${this.containername}-area`;
         this.buildGalleryFrame(container);
     }
     // ------------------------------------------------------------------------------------------------
     buildGalleryFrame(container) {
-        const div = $("<div>");
-        const galleryzone = $("<ul>").attr('id', this.galleryzone).addClass('gallery')
-        $(div).append(galleryzone);
-        $(container).append(div);
+        const galleryzone = $("<div>").addClass('galleryzone');
+        const gallerytext = $("<div>").addClass('gallerytext');
+        const spantext = $('<span>').text('Ceci est un texte bidon');
+        $(gallerytext).append(spantext);
+        const gallery = $("<ul>").attr('id', this.gallery).addClass('gallery')
+        $(galleryzone).append(gallerytext);
+        $(galleryzone).append(gallery);
+        $(container).append(galleryzone);
     }
     // ------------------------------------------------------------------------------------------------
     addImages(allimages) {
         // Get the container
-        const galleryzone = $(`#${this.galleryzone}`);
+        const gallery = $(`#${this.gallery}`);
         for(let i = 0; i < allimages.length; ++i) {
             console.log(`${this.version} Adding image ${allimages[i]} to the gallery`);
             let newimg = $('<img>').attr('src', "/images/slideshow/"+allimages[i]);
             let theline = $('<li>');
             $(theline).append(newimg);
-            $(galleryzone).append(theline);
+            $(gallery).append(theline);
         }
     }
 }
