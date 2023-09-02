6569fb82a9fa09ed683aeb07c60f1fef8fb3365b
yves40 <yves.toubhans@free.fr>
2023-09-02 09:59:02 +0200

    Update constraints list to be dropped, suppress some knife details in the form

--- src/Sql/drop-knife-constraints.sql
+++ src/Sql/drop-knife-constraints.sql
@@ -1,4 +1,6 @@
+USE BOMERLEDB;
 ALTER TABLE knifes MODIFY COLUMN stock int NULL;
 ALTER TABLE knifes MODIFY COLUMN cuttingedge_lenght int NULL;
 ALTER TABLE knifes MODIFY COLUMN lenght int NULL;
 ALTER TABLE knifes MODIFY COLUMN weight int NULL;
+ALTER TABLE knifes MODIFY COLUMN close_lenght int NULL;
