19d11bb3f4641ac4f1f16c665466e61f0d1522d6
yves40 <yves.toubhans@free.fr>
2023-09-02 18:19:57 +0200

    Cards V1 done

--- public/scripts/classes/card.js
+++ public/scripts/classes/card.js
@@ -14,17 +14,17 @@
           this.loadCard(container, data);
       }
       loadCard(container, data) {
-        console.log(`Build the card for knife : ${this.data.knifeName}`);
-        console.log(`Description is : ${this.data.knifedesc}`);
-        console.log(`Knife currently has : ${this.data.imagecount}`);
-        console.log(`First image is  : ${this.data.images[0]}`);
+        // console.log(`Build the card for knife : ${this.data.knifeName}`);
+        // console.log(`Description is : ${this.data.knifedesc}`);
+        // console.log(`Knife currently has : ${this.data.imagecount}`);
+        // console.log(`First image is  : ${this.data.images[0]}`);
         const cardimage = $("<div></div>").addClass('card-image');
         const cardtitle = $("<div></div>").addClass('card-title').html(this.data.knifeName);
         const cardtext = $("<div></div>").addClass('card-text').html(this.data.knifedesc);
         const img = $("<img>").attr('src', `/images/knife/${this.data.images[0]}`);
         $(cardimage).append(img);
-        $(container).append(cardimage);
         $(container).append(cardtitle);
+        $(container).append(cardimage);
         $(container).append(cardtext);
       }
     }
