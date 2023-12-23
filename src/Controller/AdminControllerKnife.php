efd1167945777e42c2e791bf22904b071fcf131f
yves40 <yves.toubhans@free.fr>
2023-12-23 10:31:05 +0100

    Display category name in knife list

--- src/Controller/AdminControllerKnife.php
+++ src/Controller/AdminControllerKnife.php
@@ -41,6 +41,7 @@ class AdminControllerKnife extends AbstractController
         $allknives = $repo->findBy([], ['name' => 'asc']);
 
         $form = $this->createForm(KnifesType::class, $knife);
+        dump($allknives);
         return $this->render('admin/knives.html.twig', [
             "locale" =>  $loc,
             "new" => true,
