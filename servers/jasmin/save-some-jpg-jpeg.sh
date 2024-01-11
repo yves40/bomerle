cdfe683967ae1536aa6599308feb63eefa1aab17
yves40 <yves.toubhans@free.fr>
2024-01-11 18:11:48 +0100

    typo

--- servers/jasmin/save-some-jpg-jpeg.sh
+++ servers/jasmin/save-some-jpg-jpeg.sh
@@ -1,1 +1,1 @@
-zip -@ BACKUP/some-jpg-jpeg.zip -x "*.webp" "*.png" " < $DEV/servers/jasmin/some-jpg-jpeg.list
+zip -@ BACKUP/some-jpg-jpeg.zip -x "*.webp" "*.png" < $DEV/servers/jasmin/some-jpg-jpeg.list
